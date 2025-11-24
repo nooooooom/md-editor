0f48929869fdbdfa02a5d8142848d3a2a7ba87ca
陈帅 <qixian.cs@outlook.com>
2025-11-25 02:04:37 +0800

    refactor: remove unused typewriter logic from Head and Paragraph components
    - Eliminated the typewriter state and related calculations from both Head and Paragraph components, simplifying the code.
    - Updated memoization dependencies to reflect the removal of unused variables, enhancing performance and readability.

--- src/MarkdownEditor/editor/elements/Head.tsx
+++ src/MarkdownEditor/editor/elements/Head.tsx
@@ -1,7 +1,6 @@
 import classNames from 'classnames';
-import React, { createElement, useMemo } from 'react';
+import React, { createElement } from 'react';
 import { Node } from 'slate';
-import { useSlate } from 'slate-react';
 import { ElementProps, HeadNode } from '../../el';
 import { useSelStatus } from '../../hooks/editor';
 import { useEditorStore } from '../store';
@@ -16,20 +15,8 @@ export function Head({
   const {
     store = {} as Record<string, any>,
     markdownContainerRef,
-    typewriter,
   } = useEditorStore();
-  const editor = useSlate();
   const [selected, path] = useSelStatus(element);
-
-  const isLast = useMemo(() => {
-    if (editor.children.length === 0) return false;
-    if (!typewriter) return false;
-    return store.isLatestNode(element);
-  }, [
-    editor.children.at?.(path.at(0)!),
-    editor.children.at?.(path.at(0)! + 1),
-    typewriter,
-  ]);
   const str = Node.string(element);
   return React.useMemo(() => {
     return createElement(
@@ -46,7 +33,6 @@ export function Head({
         ['data-drag-el']: true,
         className: classNames({
           empty: !str,
-          typewriter: isLast && typewriter,
         }),
       },
       <>
@@ -54,5 +40,5 @@ export function Head({
         {children}
       </>,
     );
-  }, [element.level, isLast, str, element.children, selected, path]);
+  }, [element.level, str, element.children, selected, path]);
 }
