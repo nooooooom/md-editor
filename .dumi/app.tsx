a8f9051015c8d525cf752698e76ac01f9d4efbfb
qixian <qixian.cs@antgroup.com>
2025-12-12 14:02:15 +0800

    refactor(rootContainer): 移除不必要的主题变量插入，优化组件结构

--- .dumi/app.tsx
+++ .dumi/app.tsx
@@ -1,16 +1,19 @@
-import { globalThemeToken, insertCSSVariables } from '@ant-design/theme-token';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
 import React from 'react';
 import './reset-ant.css';
 
 export function rootContainer(container: any) {
-  insertCSSVariables('ThemeExample', globalThemeToken, 'app');
-  return React.createElement(
-    ConfigProvider,
-    {
-      locale: zhCN,
-    },
-    container,
+  return (
+    <div className="one-token-vars">
+      {React.createElement(
+        ConfigProvider,
+        {
+          locale: zhCN,
+          prefixCls: 'otk',
+        },
+        container,
+      )}
+    </div>
   );
 }
