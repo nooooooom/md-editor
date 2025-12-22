6fc390ad59b5edd57c1966b90cf3f3409069140d
qixian.cs <qixian.cs@outlook.com>
2025-12-22 10:55:49 +0800

    feat(dumirc): add Google Tag Manager script for enhanced tracking
    - Integrated Google Tag Manager by adding the necessary script and initialization code to the configuration.
    - This addition enables improved tracking and analytics capabilities for the application.

--- .dumirc.ts
+++ .dumirc.ts
@@ -14,4 +14,16 @@ export default defineConfig({
   resolve: {
     docDirs: ['docs', 'src/schema'],
   },
+  scripts: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-8V1D6XCMW3',
+      async: true,
+    },
+    `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-8V1D6XCMW3');
+    `,
+  ],
 });
