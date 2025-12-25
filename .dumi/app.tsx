44a610156b6fce5e58973780a039f959d0689d80
qixian.cs <qixian.cs@outlook.com>
2025-12-25 20:30:16 +0800

    feat(AppWrapper): add AppWrapper component to utilize useAppData and log app data on mount

--- .dumi/app.tsx
+++ .dumi/app.tsx
@@ -1,7 +1,19 @@
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
-import React from 'react';
+import { useAppData } from 'dumi';
+import React, { useEffect } from 'react';
 import './reset-ant.css';
+
+// 包装组件，用于在应用级别使用 useAppData
+const AppWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const appdata = useAppData();
+
+  useEffect(() => {
+    console.log('appdata:', appdata);
+  }, [appdata]);
+
+  return <>{children}</>;
+};
 
 export function rootContainer(container: any) {
   return React.createElement(
@@ -10,6 +22,6 @@ export function rootContainer(container: any) {
       locale: zhCN,
       prefixCls: 'otk',
     },
-    container,
+    React.createElement(AppWrapper, null, container),
   );
 }
